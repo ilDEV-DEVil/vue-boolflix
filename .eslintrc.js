5cf5bd7e3cb9688872b7349e899690df9fa2f5bb
flashbLack98 <npalakuttige98@icloud.com>
2021-11-17 15:06:07 +0100

    start

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,20 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
+  "root": true,
+  "env": {
+    "node": true
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  "extends": [
+    "plugin:vue/essential",
+    "eslint:recommended"
   ],
-  parserOptions: {
-    parser: 'babel-eslint'
+  "parserOptions": {
+    "parser": "babel-eslint"
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  "rules": {
+    "no-console": "off",
+    "no-debugger": "off"
+  },
+  "ignorePatterns": [
+    "dist/*"
+  ]
 }
